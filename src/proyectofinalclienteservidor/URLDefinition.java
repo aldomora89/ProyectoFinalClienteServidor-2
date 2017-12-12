fa4b665ab1de43c520ad4cabeead7005d2eccbb6
Santiago Méndez Castillo <imendezc976@ulacit.ed.cr>
2017-12-11 20:57:14 -0600

    Se agregan JAR y URLDefinition

---
+++ src/proyectofinalclienteservidor/URLDefinition.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinalclienteservidor;
+
+/**
+ *
+ * @author ulacit
+ */
+public enum URLDefinition {
+
+    Cliente("Cliente"), message("Message");
+
+    private String url;
+    private String baseURL = "http://localhost:8080/ServidorRest/webresources/";
+
+    private URLDefinition(final String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return baseURL + url;
+    }
+
+}
